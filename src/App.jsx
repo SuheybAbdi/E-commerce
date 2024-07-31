291ac9d8a5597a31d119aa5a3130720edade4532
SuheybAbdi <166018953+SuheybAbdi@users.noreply.github.com>
2024-07-31 09:39:00 +0300

    commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,86 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React, { useState } from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Header from "./components/Header"; // Corrected path
+import Product from "./pages/Product";
+import SingleProduct from "./components/SingleProduct"; // Corrected path
+import Home from "./pages/Home"; // Corrected path
+import About from "./pages/About"; // Corrected path
+import NotFound from "./components/NotFound"; // Corrected path
+import "bootstrap/dist/css/bootstrap.min.css"; // Corrected import path
+import ShoppingCart from "./components/ShoppingCart"; // Corrected path
+import { ToastContainer, toast } from "react-toastify"; // Corrected import path
+import "react-toastify/dist/ReactToastify.css"; // Corrected import path
+import Contact from "./pages/Contact"; // Corrected path
+import GoToCheckout from "./pages/GoToChekout";
+import GoToTop from "./components/GoToTop"; // Corrected path
+import Footer from "./components/Footer";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [cart, setCart] = useState([]);
+
+  const AddToCart = (data) => {
+    const itemExists = cart.some((item) => item.id === data.id);
+
+    if (itemExists) {
+      toast.error("This item is already in your cart.");
+      return;
+    }
+
+    setCart([...cart, { ...data, quantity: 1 }]);
+    toast.success("Item added to cart.");
+  };
+
+  const handleRemoveItem = (itemId) => {
+    const filteredData = cart.filter((item) => item.id !== itemId);
+    if (filteredData.length === cart.length) {
+      toast.error("Failed to remove item.");
+    } else {
+      setCart(filteredData);
+      toast.success("Item removed from cart.");
+    }
+  };
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <BrowserRouter>
+        <Header count={cart.length} ShoppingCart={<ShoppingCart />} />
+
+        <Routes>
+          <Route index element={<Home />} />
+          <Route path="/home" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/products" element={<Product />} />
+          <Route path="/contact" element={<Contact />} />
+          <Route path="/goToCheckout" element={<GoToCheckout />} />
+          <Route
+            path="/products/:id"
+            element={
+              <SingleProduct
+                AddToCart={AddToCart}
+                ToastContainer={ToastContainer}
+                toast={toast}
+              />
+            }
+          />
+          <Route
+            path="/shoppingCart/:id"
+            element={
+              <ShoppingCart
+                cart={cart}
+                handleRemoveItem={handleRemoveItem}
+                count={cart.length}
+                ToastContainer={<ToastContainer />}
+              />
+            }
+          />
+          <Route path="*" element={<NotFound />} />
+        </Routes>
+        <GoToTop />
+        <Footer />
+        <ToastContainer />
+      </BrowserRouter>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,16 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import { Auth0Provider } from "@auth0/auth0-react";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <Auth0Provider
+    domain="dev-cvs0qpquj6r8pj4w.us.auth0.com"
+    clientId="kG2Wt2N5pyNr1xMBOSB2bnhr0hgKWyxY"
+    authorizationParams={{
+      redirect_uri: window.location.origin,
+    }}
+  >
     <App />
-  </React.StrictMode>,
-)
+  </Auth0Provider>
+);
